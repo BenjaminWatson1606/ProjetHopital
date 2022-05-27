05224a0ff472ce150de14d681f61ed2d4fe87312
JulienVanh <julien.vanhauwaert@gmail.com>
2022-05-28 01:13:33 +0200

    Mofication Navbar

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -38,19 +38,16 @@ const NavBar = ({ isAuthenticated, setIsAuthenticated }) => {
           </header>
           <div className="l-navbar" id="nav-bar">
               <nav className="nav">
-                  <div> <a href="http://localhost:8000{{ path('accueil') }}" className="nav_logo"> <i className='bx bx-layer nav_logo-icon'></i> <span
-                      className="nav_logo-name">Accueil</span> </a>
+                  <div> 
+                    <a href="." className="nav_logo"> <i className='bx bx-layer nav_logo-icon'></i> <span className="nav_logo-name">Accueil</span> </a>
 
-                      <div className="nav_list"> <a href="http://127.0.0.1:8000{{ path('hospitalisation_index') }}" className="nav_link active"> <i className='bx bx-grid-alt nav_icon'></i>
-                          <span className="nav_name">Hospitalisation</span> </a> <a href="http://localhost:8000{{ path('patient_index') }}" className="nav_link"> <i
-                              className='bx bx-user nav_icon'></i> <span className="nav_name">Patients</span> </a> <a href="http://localhost:8000{{ path('service_index') }}"
-                                  className="nav_link"> <i className='bx bx-message-square-detail nav_icon'></i> <span
-                                      className="nav_name">Services</span> </a> <a href="http://localhost:8000{{ path('chambre_index') }}" className="nav_link"> <i
-                                          className='bx bx-bookmark nav_icon'></i> <span className="nav_name">Chambres</span> </a> <a href="http://localhost:8000{{ path('compte_index') }}" className="nav_link"></a> <i
-                                              className='bx bx-user nav_icon'></i> <span className="nav_name">Comptes</span>
-
-
-                          </div>
+                      <div className="nav_list"> 
+                          <a href="." className="nav_link active"> <i className='bx bx-grid-alt nav_icon'></i> <span className="nav_name">Patient</span> </a> 
+                          <a href="." className="nav_link"> <i className='bx bx-user nav_icon'></i> <span className="nav_name">Lit</span> </a> 
+                          <a href="."className="nav_link"> <i className='bx bx-message-square-detail nav_icon'></i> <span className="nav_name">Chambre</span> </a> 
+                          <a href="." className="nav_link"> <i className='bx bx-bookmark nav_icon'></i> <span className="nav_name">Service</span> </a> 
+                          <a href="." className="nav_link"> <i className='bx bx-user nav_icon'></i> <span className="nav_name">Comptes</span></a>
+                      </div>
                     
                   </div> <a href="http://localhost:8000/logout" className="nav_link"> <i className='bx bx-log-out nav_icon'></i> <span className="nav_name">Se déconnecter</span> </a>
               </nav>
---
+++ assets/js/pages/Patient.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useEffect } from 'react';
+import AuthAPI from "../services/authAPI";
+import { useTranslation } from "react-i18next";
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    Modal,
+    Row,
+    Col
+  } from "reactstrap";
+
+  const Patient = (props) => {
+    
+    return (
+        <>
+            <div>Coucouuuuuuuuuuuuuu</div>
+
+
+            <div>Coucouuuuuuuuuuuuuu</div>
+            <div>Coucouuuuuuuuuuuuuu</div>
+            <div>Coucouuuuuuuuuuuuuu</div>
+            <div>Coucouuuuuuuuuuuuuu</div>
+            <div>CoucouuuuuuuIBYUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGuuuuuuu</div>
+        </>
+    )
+
+  }
+
+export default Patient;
