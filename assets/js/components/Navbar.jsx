8e7fbf3802a8ccbee927e64992f47a2ac85e3695
Benjamin Watson <benjamin.watson1606@gmail.com>
2022-05-30 03:08:13 +0200

    Remise d'image navbar

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -36,7 +36,7 @@ const NavBar = ({ isAuthenticated, setIsAuthenticated }) => {
       <div id="body-pd">
           <header className="header" id="header">
               <div className="header_toggle"> <i className='bx bx-menu' id="header-toggle"></i> </div>
-              
+              <div className="header_img"> <img src={logo} className='header_img' alt="logo"></img> </div>
           </header>
           <div className="l-navbar" id="nav-bar">
               <nav className="nav">
